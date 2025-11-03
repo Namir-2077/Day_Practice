e4adbcd21c51abc6fa1e80a5c727f7179c529048
Namir-2077 <namir2077.be23@chitkara.edu.in>
2025-11-03 10:55:19 +0530

    Updated

---
+++ Day_Practice_1/Day_Practice_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day_Practice_1
+//
+//  Created by Student on 03/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Day_Practice_1/Day_Practice_1Tests/Day_Practice_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Day_Practice_1Tests.swift
+//  Day_Practice_1Tests
+//
+//  Created by Student on 03/11/25.
+//
+
+import Testing
+@testable import Day_Practice_1
+
+struct Day_Practice_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
